ac41fbd44539c9c2feb4beb707821047e6e27416
SergioGBosco <sergiogibosco@gmail.com>
2025-07-12 15:49:17 +0200

    fix: edit layout ticket

--- js/script.js
+++ js/script.js
@@ -32,35 +32,63 @@ check.addEventListener(`click`, function (e) {
 
   //Variabile vuota creata per inserire il prezzo finale al netto di sconti
   let finalprice;
+  let runoffer;
 
   //aggiungo la condizione 
   if (age < 18) {
     finalprice = runprice * 0.8 //sconto del 20%
-
+    runoffer = `Tariffa Ridotta`
   }
-  else if (age >= 65)
+  else if (age >= 65) {
     finalprice = runprice * 0.60 //sconto del 40%
-
+    runoffer = `Tariffa Ridotta`
+  }
   else {
     finalprice = runprice
+    runoffer = `Tariffa Standard`
   }
 
   //Mostro il risultato direttamente in pagina, quindi recupero prima la variabile da html 
 
-  result.innerText = `Ciao ${iduser}, in base alla tua età ${age} e la distanza da percorrere ${distance} il prezzo del tuo biglietto è ${finalprice.toFixed(2)} `
+  // result.innerText = `Ciao ${iduser}, in base alla tua età ${age} e la distanza da percorrere ${distance} il prezzo del tuo biglietto è ${finalprice.toFixed(2)} `;
+
+  //creo delle variabili per formare il biglietto
+
+  const offer = document.getElementById(`offer`);
+  const wagonnumber = document.getElementById(`wagonnumber`);
+  const codrandom = document.getElementById(`codrandom`);
+  const finalpricerun = document.getElementById(`finalpricerun`);
+
+  offer.innerText = runoffer
+
+  wagonnumber.innerText = Math.floor(Math.random() * 10 + 1);
+
+  codrandom.innerText = Math.floor(Math.random() * 10000 + 1);
+
+  finalpricerun.innerText = `${finalprice.toFixed(2)} €`;
+
 })
 
 reset.addEventListener(`click`, function (e) {
   e.preventDefault();
   result.innerText = ``;
 
+
+
+
   //prendo le variabili dal dom ma non richiamandole con le variabili assegnate, perchè essendo all'inteno di un altra funzione queste non vengono visualizzate 
 
   document.getElementById("nominativo").value = ``;
   document.getElementById("km").value = ``;
   document.getElementById("age").value = ``;
-
+  document.getElementById("offer").value = ``;
+  document.getElementById("wagonnumber").value = ``;
+  document.getElementById("codrandom").value = ``;
+  document.getElementById("finalpricerun").value = ``;
 })
+
+
+
 
 
 
